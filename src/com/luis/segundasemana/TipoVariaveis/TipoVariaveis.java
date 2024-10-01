3d0a74420127649095038bb5266511ce158021a3
luis lemos <luislemosm12@gmail.com>
2024-09-30 21:14:28 -0300

    subindo exemplos de tipos e variáveis.

--- src/com/luis/segundasemana/TipoVariaveis/TipoVariaveis.java
+++ src/com/luis/segundasemana/TipoVariaveis/TipoVariaveis.java
@@ -1,5 +1,14 @@
 package com.luis.segundasemana.TipoVariaveis;
 
 public class TipoVariaveis {
+    public static void main(String[] args) {
+        double salarioMinimo = 2.500;
+        
+        System.out.println(" O salario minimo é de: " + salarioMinimo);
+
+        int anoDeFabricacao= 2021;
+
+        System.out.println("Então eu posso comprar um carro do ano de fabricação: " + anoDeFabricacao);
+    }
 
 }
