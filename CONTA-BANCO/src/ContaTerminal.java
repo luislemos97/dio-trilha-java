a8e21730c0bdfaf84932ec37fca267b2a606a1b9
luis lemos <luislemosm12@gmail.com>
2024-10-19 10:44:37 -0300

    Adicionando projeto de exercio em java

---
+++ CONTA-BANCO/src/ContaTerminal.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner (System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o seu nome: ");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Por favor, digite o número da agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite o número da conta: ");
+        int numeroDaConta = scanner.nextInt();
+
+        System.out.println("Saldo Disponível: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente +
+         " , obrigado por criar uma conta em nosso banco, sua agência é: " + agencia +
+         ", conta: " + numeroDaConta+ " e seu saldo: " + saldo + " já está disponível para saque! ");
+
+    }
+}
