16e1a06d3302cc0ba91c58e29a5c58f352be42e9
luis lemos <luislemosm12@gmail.com>
2024-10-20 21:48:25 -0300

    estudos e exercicios sobre Estrutura condicionais

---
+++ src/com/luis/controledefluxo/CondicaoTernaria.java
@@ -0,0 +1,13 @@
+package com.luis.controledefluxo;
+
+public class CondicaoTernaria {
+
+    public static void main(String[] args) {
+        
+        int nota = 6;
+        String  resultado = nota >=7 ? "Aprovado" : nota >=5  && nota <7 ? "Prova de recuperação" : "Reprovado";
+        System.out.println(resultado);
+        
+    }
+
+}
