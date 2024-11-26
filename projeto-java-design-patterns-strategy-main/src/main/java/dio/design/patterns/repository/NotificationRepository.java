fbfcb3e40716ef121039d39ea55e8fb6739be25b
Luis Lemos <luislemosm12@gmail.com>
2024-11-26 09:57:25 -0300

    ajuste no projeto

--- projeto-java-design-patterns-strategy-main/src/main/java/dio/design/patterns/repository/NotificationRepository.java
+++
@@ -1,7 +0,0 @@
-package dio.design.patterns.repository;
-
-import dio.design.patterns.entity.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
-}
