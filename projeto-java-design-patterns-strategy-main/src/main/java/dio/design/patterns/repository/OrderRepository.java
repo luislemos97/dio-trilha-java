6dd409c2a1d9fe54ba3f984b47cb0fcd4421c61e
Luis Lemos <luis.lemos@letwe.com.br>
2024-11-26 09:57:25 -0300

    ajuste no projeto

--- projeto-java-design-patterns-strategy-main/src/main/java/dio/design/patterns/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package dio.design.patterns.repository;
-
-import dio.design.patterns.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
