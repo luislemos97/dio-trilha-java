85eee9086bc609a6dadc5555e62ccd80c0794d82
Luis Lemos <luis.lemos@letwe.com.br>
2024-12-04 23:43:40 -0300

    delete in project's

--- iphone-servicos/src/com/luis/reprodutor/ReprodutorMusical.java
+++
@@ -1,7 +0,0 @@
-package com.luis.reprodutor;
-
-public interface ReprodutorMusical {
-    void play();
-    void pause();
-    void selectTrack(String track);
-}
--- projeto-java-design-patterns-strategy-main/src/main/java/dio/design/patterns/strategy/PushNotification.java
+++
@@ -1,8 +0,0 @@
-package dio.design.patterns.strategy;
-
-public class PushNotification implements NotificationStrategy{
-    @Override
-    public void sendNotification(String message) {
-        System.out.println("Sending push notification " + message);
-    }
-}
